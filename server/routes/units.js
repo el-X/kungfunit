72d1e679259bca75da0f28e54c7c96fa1ee84a5d
Alexander Keller <a.k3ll3r@gmail.com>
2016-05-16 22:40:32 +0200

    Create route for available quantities and units

--- server/routes/units.js
+++ server/routes/units.js
@@ -5,13 +5,95 @@ var router = express.Router();
 var Qty = require('js-quantities');
 var math = require('mathjs');
 
-/* GET the conversion for a certain unit. */
+/**
+ * GET all available quantities and their corresponding units.
+ **/
+router.get('/', function (req, res) {
+    var quantities = {
+        "quantities": [
+            {
+                "quantity": {
+                    "name": "Length",
+                    "units": [
+                        {
+                            "unit": {
+                                "name": "Kilometre",
+                                "symbol": "km"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Metre",
+                                "symbol": "m"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Centimetre",
+                                "symbol": "cm"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Millimetre",
+                                "symbol": "mm"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Micrometre",
+                                "symbol": "um"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Nanometre",
+                                "symbol": "nm"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Mile",
+                                "symbol": "mi"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Yard",
+                                "symbol": "yd"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Foot",
+                                "symbol": "ft"
+                            }
+                        },
+                        {
+                            "unit": {
+                                "name": "Inch",
+                                "symbol": "in"
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    };
+
+    var response = {"data": quantities};
+    res.json(response);
+});
+
+/**
+ * GET the conversion for a certain unit.
+ **/
 router.get('/convert', function (req, res) {
-    var quantity = req.query.q;
+    var query = req.query.q;
     var source = req.query.source;
     var target = req.query.target;
 
-    var from = math.unit(quantity, source);
+    var from = math.unit(query, source);
     var to = from.toNumber(target);
 
     var response = {
