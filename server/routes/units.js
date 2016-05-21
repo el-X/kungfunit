4b852306cb5270c0ddf8d9c028978da525eda00d
Alexander Keller <a.k3ll3r@gmail.com>
2016-05-21 18:58:07 +0200

    Add a class definition to the quantities

--- server/routes/units.js
+++ server/routes/units.js
@@ -9,7 +9,7 @@ var math = require('mathjs');
  * GET all available quantities and their corresponding units.
  **/
 router.get('/', function (req, res) {
-    var response = {"data": getQuantities()};
+    var response = {"data": getAvailableUnits()};
     res.json(response);
 });
 
@@ -35,172 +35,220 @@ router.get('/convert', function (req, res) {
     res.json(response);
 });
 
-function getQuantities() {
+function getAvailableUnits() {
     return {
-        "quantities": [
+        "classes": [
             {
+                "name": "SI base units",
+                "quantities": [
+                    {
 
-                "name": "Length",
-                "units": [
-                    {
-                        "name": "Kilometre",
-                        "symbol": "km"
+                        "name": "Length",
+                        "units": [
+                            {
+                                "name": "Kilometre",
+                                "symbol": "km"
+                            },
+                            {
+                                "name": "Metre",
+                                "symbol": "m"
+                            },
+                            {
+                                "name": "Centimetre",
+                                "symbol": "cm"
+                            },
+                            {
+                                "name": "Millimetre",
+                                "symbol": "mm"
+                            },
+                            {
+                                "name": "Micrometre",
+                                "symbol": "um"
+                            },
+                            {
+                                "name": "Nanometre",
+                                "symbol": "nm"
+                            },
+                            {
+                                "name": "Mile",
+                                "symbol": "mi"
+                            },
+                            {
+                                "name": "Yard",
+                                "symbol": "yd"
+                            },
+                            {
+                                "name": "Foot",
+                                "symbol": "ft"
+                            },
+                            {
+                                "name": "Inch",
+                                "symbol": "in"
+                            }
+                        ]
                     },
-                    {
-                        "name": "Metre",
-                        "symbol": "m"
-                    },
-                    {
-                        "name": "Centimetre",
-                        "symbol": "cm"
-                    },
-                    {
-                        "name": "Millimetre",
-                        "symbol": "mm"
-                    },
-                    {
-                        "name": "Micrometre",
-                        "symbol": "um"
-                    },
-                    {
-                        "name": "Nanometre",
-                        "symbol": "nm"
-                    },
-                    {
-                        "name": "Mile",
-                        "symbol": "mi"
-                    },
-                    {
-                        "name": "Yard",
-                        "symbol": "yd"
-                    },
-                    {
-                        "name": "Foot",
-                        "symbol": "ft"
-                    },
-                    {
-                        "name": "Inch",
-                        "symbol": "in"
-                    }
-                ]
-            },
 
-            {
-                "name": "Mass",
-                "units": [
                     {
-                        "name": "Tonne",
-                        "symbol": "tonne"
+                        "name": "Mass",
+                        "units": [
+                            {
+                                "name": "Tonne",
+                                "symbol": "tonne"
+                            },
+                            {
+                                "name": "Kilogram",
+                                "symbol": "kg"
+                            },
+                            {
+                                "name": "Gram",
+                                "symbol": "g"
+                            },
+                            {
+                                "name": "Milligram",
+                                "symbol": "mg"
+                            },
+                            {
+                                "name": "Microgram",
+                                "symbol": "ug"
+                            },
+                            {
+                                "name": "US ton",
+                                "symbol": "ton"
+                            },
+                            {
+                                "name": "Stone",
+                                "symbol": "stone"
+                            },
+                            {
+                                "name": "Pound",
+                                "symbol": "lb"
+                            },
+                            {
+                                "name": "Ounce",
+                                "symbol": "oz"
+                            }
+                        ]
                     },
                     {
-                        "name": "Kilogram",
-                        "symbol": "kg"
+                        "name": "Time",
+                        "units": [
+                            {
+                                "name": "Nanosecond",
+                                "symbol": "ns"
+                            },
+                            {
+                                "name": "Microsecond",
+                                "symbol": "us"
+                            },
+                            {
+                                "name": "Millisecond",
+                                "symbol": "ms"
+                            },
+                            {
+                                "name": "Second",
+                                "symbol": "s"
+                            },
+                            {
+                                "name": "Minutes",
+                                "symbol": "mins"
+                            },
+                            {
+                                "name": "Hour",
+                                "symbol": "h"
+                            },
+                            {
+                                "name": "Day",
+                                "symbol": "days"
+                            },
+                            {
+                                "name": "Week",
+                                "symbol": "weeks"
+                            },
+                            {
+                                "name": "Month",
+                                "symbol": "months"
+                            },
+                            {
+                                "name": "Year",
+                                "symbol": "years"
+                            },
+                            {
+                                "name": "Decade",
+                                "symbol": "decades"
+                            },
+                            {
+                                "name": "Century",
+                                "symbol": "centuries"
+                            },
+                            {
+                                "name": "Millennium",
+                                "symbol": "millennia"
+                            }
+                        ]
                     },
                     {
-                        "name": "Gram",
-                        "symbol": "g"
-                    },
-                    {
-                        "name": "Milligram",
-                        "symbol": "mg"
-                    },
-                    {
-                        "name": "Microgram",
-                        "symbol": "ug"
-                    },
-                    {
-                        "name": "US ton",
-                        "symbol": "ton"
-                    },
-                    {
-                        "name": "Stone",
-                        "symbol": "stone"
-                    },
-                    {
-                        "name": "Pound",
-                        "symbol": "lb"
-                    },
-                    {
-                        "name": "Ounce",
-                        "symbol": "oz"
+                        "name": "Temperature",
+                        "units": [
+                            {
+                                "name": "Celsius",
+                                "symbol": "degC"
+                            },
+                            {
+                                "name": "Fahrenheit",
+                                "symbol": "degF"
+                            },
+                            {
+                                "name": "Kelvin",
+                                "symbol": "K"
+                            },
+                            {
+                                "name": "Rankine",
+                                "symbol": "degR"
+                            }
+                        ]
                     }
                 ]
             },
             {
-                "name": "Time",
-                "units": [
+                "name": "SI derived units",
+                "quantities": [
                     {
-                        "name": "Nanosecond",
-                        "symbol": "ns"
-                    },
-                    {
-                        "name": "Microsecond",
-                        "symbol": "us"
-                    },
-                    {
-                        "name": "Millisecond",
-                        "symbol": "ms"
-                    },
-                    {
-                        "name": "Second",
-                        "symbol": "s"
-                    },
-                    {
-                        "name": "Minutes",
-                        "symbol": "mins"
-                    },
-                    {
-                        "name": "Hour",
-                        "symbol": "h"
-                    },
-                    {
-                        "name": "Day",
-                        "symbol": "days"
-                    },
-                    {
-                        "name": "Week",
-                        "symbol": "weeks"
-                    },
-                    {
-                        "name": "Month",
-                        "symbol": "months"
-                    },
-                    {
-                        "name": "Year",
-                        "symbol": "years"
-                    },
-                    {
-                        "name": "Decade",
-                        "symbol": "decades"
-                    },
-                    {
-                        "name": "Century",
-                        "symbol": "centuries"
-                    },
-                    {
-                        "name": "Millennium",
-                        "symbol": "millennia"
-                    }
-                ]
-            },
-            {
-                "name": "Temperature",
-                "units": [
-                    {
-                        "name": "Celsius",
-                        "symbol": "degC"
-                    },
-                    {
-                        "name": "Fahrenheit",
-                        "symbol": "degF"
-                    },
-                    {
-                        "name": "Kelvin",
-                        "symbol": "K"
-                    },
-                    {
-                        "name": "Rankine",
-                        "symbol": "degR"
+
+                        "name": "Area",
+                        "units": [
+                            {
+                                "name": "Square kilometre",
+                                "symbol": "km2"
+                            },
+                            {
+                                "name": "Square metre",
+                                "symbol": "m2"
+                            },
+                            {
+                                "name": "Square mile",
+                                "symbol": "sqmi"
+                            },
+                            {
+                                "name": "Square yard",
+                                "symbol": "sqyd"
+                            },
+                            {
+                                "name": "Square foot",
+                                "symbol": "sqft"
+                            },
+                            {
+                                "name": "Square inch",
+                                "symbol": "sqin"
+                            },
+                            {
+                                "name": "Hectare",
+                                "symbol": "hectare"
+                            },
+                            {
+                                "name": "Acre",
+                                "symbol": "acre"
+                            }
+                        ]
                     }
                 ]
             }
