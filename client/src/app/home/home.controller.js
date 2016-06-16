258ac1c2fbc43292b27e305b860b2437ca63af4a
Alexander Keller <a.k3ll3r@gmail.com>
2016-06-17 00:18:27 +0200

    Get a lot of things done
    - Filtering, adding and removing converters is now possible
    - Added a dummy logger
    - Converters themselves are now closeable
    - Stylized some text with a newly added font
    - Added an about page
    - Extracted a few inline-styles to (S)CSS

--- client/src/app/home/home.controller.js
+++ client/src/app/home/home.controller.js
@@ -12,16 +12,21 @@
         vm.unitClasses = [];
         vm.unitQuantities = [];
         vm.unitQuantityNames = [];
-        vm.selectedUnitQuantities = ["Length", "Time", "Area", "Mass", "Temperature"];
+        vm.selectedUnitQuantities = ["Length", "Time", "Temperature"];
         vm.searchTerm = "";
         vm.toggle = false;
+        vm.logMessage = "2016.05.19: 13:37 - Currency: 1 Bitcoin (BTC) equals 392.16 Euro (EUR) on 2016.05.19";
 
         vm.convertFromLeftToRight = convertFromLeftToRight;
         vm.convertFromRightToLeft = convertFromRightToLeft;
         vm.isUnitQuantitySelected = isUnitQuantitySelected;
+        vm.toggleConverter = toggleConverter;
 
         retrieveUnitClasses();
 
+        /**
+         * Retrives the unit classes and its contained quantities via REST.
+         */
         function retrieveUnitClasses() {
             unitService.getUnits({}, function (unitClasses) {
                 vm.unitClasses = unitClasses.data.classes;
@@ -34,6 +39,11 @@
             });
         }
 
+        /**
+         * Performs a conversion using the left hand side value with the right hand side value as the result.
+         *
+         * @param converterScope contains all parameters necessary for the conversion
+         */
         function convertFromLeftToRight(converterScope) {
             unitService.convert({
                 q: converterScope.lhsValue,
@@ -44,6 +54,11 @@
             });
         }
 
+        /**
+         * Performs a conversion using the right hand side value with the left hand side value as the result.
+         *
+         * @param converterScope contains all parameters necessary for the conversion
+         */
         function convertFromRightToLeft(converterScope) {
             unitService.convert({
                 q: converterScope.rhsValue,
@@ -54,8 +69,28 @@
             });
         }
 
-        function isUnitQuantitySelected(unitQuantity) {
-            return lodash.includes(vm.selectedUnitQuantities, unitQuantity);
+        /**
+         * Determines whether or not a certain unit quantity is selected, so that its corresponding converter is displayed.
+         *
+         * @param unitQuantityName the name of the quantity whose selection should be checked
+         * @returns true if the quantity name is present; false otherwise
+         */
+        function isUnitQuantitySelected(unitQuantityName) {
+            return lodash.includes(vm.selectedUnitQuantities, unitQuantityName);
         }
+
+        /**
+         * Toggles the selection of a converter using its quantity name.
+         *
+         * @param unitQuantityName the name of the quantity whose converter should be either displayed or not
+         */
+        function toggleConverter(unitQuantityName) {
+            if (lodash.includes(vm.selectedUnitQuantities, unitQuantityName)) {
+                lodash.pull(vm.selectedUnitQuantities, unitQuantityName);
+            } else {
+                vm.selectedUnitQuantities.push(unitQuantityName);
+            }
+        }
+
     }
 })();
