0aab484e5edfdc734737f508acd99af8b9cd98a6
Alexander Keller <a.k3ll3r@gmail.com>
2016-07-06 00:14:57 +0200

    Conclude dataset conversion
    - Aligned the datepicker properly
    - Made the datepicker only appear if currencies are selected
    - Added dataset validation mechanisms

--- client/src/app/datasets/datasets.controller.js
+++ client/src/app/datasets/datasets.controller.js
@@ -6,13 +6,15 @@
         .controller('DatasetsController', DatasetsController);
 
     /** @ngInject */
-    function DatasetsController(unitService, lodash) {
+    function DatasetsController(unitService, lodash, moment) {
         var vm = this;
 
         vm.date = moment().toDate();
         vm.minDate = moment().subtract(4, 'days').toDate(); // only five days should be selectable
         vm.maxDate = moment().toDate();
+        vm.invalid = false;
         vm.unitClasses = [];
+        vm.selectedQuantity = "";
         vm.selectedSourceUnit = "";
         vm.selectedTargetUnit = "";
         vm.sourceDataset = "";
@@ -32,24 +34,41 @@
         }
 
         function convertDataset() {
-            var separator = ",";
-            var trimmer = separator + " ";
+            var values = lodash.split(vm.sourceDataset, ",");
 
-            var values = lodash.trim(vm.sourceDataset, trimmer);
-            values = lodash.split(values, separator);
+            if (isValid(values)) {
+                retrieveConversion(values);
+            }
+        }
 
-            console.log(values);
+        function isValid(values) {
+            vm.invalid = lodash.some(values, function (value) {
+                var empty = !(/\S/.test(value));
+                return empty || lodash.isNaN(Number(value));
+            });
 
+            return !vm.invalid;
+        }
+
+        function retrieveConversion(values) {
             unitService.convert({
                 q: values,
                 source: vm.selectedSourceUnit,
                 target: vm.selectedTargetUnit,
-                date: undefined
+                date: formatDate(vm.date)
             }, function (conversions) {
                 var filteredConversions = lodash.map(conversions.data.conversions, 'convertedUnit');
-                console.log(filteredConversions);
-                vm.targetDataset = lodash.join(filteredConversions, trimmer);
+                vm.targetDataset = lodash.join(filteredConversions, ", ");
             });
         }
+
+        /**
+         * Formats the date in order for its currency to be convertible. If no date is specified "null" will be
+         * returned.
+         */
+        function formatDate(date) {
+            return vm.selectedQuantity.name == 'Currency' ? moment(date).format("YYYY-MM-DD") : null;
+        }
+
     }
 })();
